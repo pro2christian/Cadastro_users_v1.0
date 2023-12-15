41d43e69b5be1695cd9476840d09c50f3ca397bf
Christian Oliveira da Silva <pro2christian@icloud.com>
2023-12-15 13:15:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Projeto_para_estudos
+{
+    class Program
+    {
+
+        class CadastroUsers
+        {
+            static string deLimitdorInicio;
+            static string deLimitdorFim;
+            static string tgNome;
+            static string tgDtNascimento;
+            static string tgNomeDaRua;
+            static string tgNumeroCasa;
+
+            public struct DadosDeUsuario_e
+            {
+                public string Nome;
+                public DateTime DataDeNascimento;
+                public string NomeDaRua;
+                public uint NumeroDaCasa;
+            }
+            public enum Resultado_e
+            {
+                Sucesso = 0,
+                Sair = 1,
+                Excecao = 2
+            }
+            //métodos:
+            public static void ImprimeMensagens(string mensagem)
+            {
+                Console.WriteLine(mensagem);
+            }
+            public static Resultado_e PegaString(ref string minhaString, string mensagemNoMenu)
+            {
+                string temp = "";
+
+                Resultado_e retorno;
+                do
+                {
+                    Console.WriteLine(mensagemNoMenu);
+                    temp = Console.ReadLine();
+                    if (temp == string.Empty)
+                    {
+                        ImprimeMensagens("Nenhum nome digitado!!");
+                        Console.ReadKey();
+                    }
+                    Console.Clear();
+                } while (string.IsNullOrEmpty(temp));
+
+                if (temp == "s" || temp == "S")
+                    retorno = Resultado_e.Sair;
+                else
+                {
+                    minhaString = temp;
+                    retorno = Resultado_e.Sucesso;
+                }
+                Console.Clear();
+                return retorno;
+            }
+            public static Resultado_e PegaData(ref DateTime data, string mensagemNoMenu)
+            {
+                Resultado_e retorno;
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine(mensagemNoMenu);
+                        string temp = Console.ReadLine();
+                        if (temp == "s" || temp == "S")
+
+                            retorno = Resultado_e.Sair;
+                        else
+                        {
+                            data = Convert.ToDateTime(temp);
+                            retorno = Resultado_e.Sucesso;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exceção: " + ex.Message);
+                        ImprimeMensagens("Presione qualquer tecla para continuar");
+                        Console.ReadKey();
+                        Console.Clear();
+                        retorno = Resultado_e.Excecao;
+                    }
+
+                } while (retorno == Resultado_e.Excecao);
+                Console.Clear();
+                return retorno;
+            }
+            public static Resultado_e PegaNumeroCasa(ref uint numeroCasa, string mensagemNoMenu)
+            {
+                Resultado_e retorno;
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine(mensagemNoMenu);
+                        string temp = Console.ReadLine();
+                        if (temp == "s" || temp == "S")
+
+                            retorno = Resultado_e.Sair;
+                        else
+                        {
+                            numeroCasa = Convert.ToUInt32(temp);
+                            retorno = Resultado_e.Sucesso;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exeção: " + ex.Message);
+                        ImprimeMensagens("Presione qualquer tecla para continuar");
+                        Console.ReadKey();
+                        Console.Clear();
+                        retorno = Resultado_e.Excecao;
+                    }
+
+                } while (retorno == Resultado_e.Excecao);
+                Console.Clear();
+                return retorno;
+            }
+            public static Resultado_e CadastraUsuario(ref List<DadosDeUsuario_e> ListaUsuario_m)
+            {
+                DadosDeUsuario_e cadastroDoUsuario;
+                cadastroDoUsuario.Nome = "";
+                cadastroDoUsuario.DataDeNascimento = new DateTime();
+                cadastroDoUsuario.NomeDaRua = "";
+                cadastroDoUsuario.NumeroDaCasa = 0;
+
+                if (PegaString(ref cadastroDoUsuario.Nome, "Digite o nome completo ou S para sair") == Resultado_e.Sair)
+                    return Resultado_e.Sair;
+                if (PegaData(ref cadastroDoUsuario.DataDeNascimento, "Digite a data de nascimento no formato DD/MM/YYYY ou S para sair") == Resultado_e.Sair)
+                    return Resultado_e.Sair;
+                if (PegaString(ref cadastroDoUsuario.NomeDaRua, "Digite o nome da rua ou S para sair") == Resultado_e.Sair)
+                    return Resultado_e.Sair;
+                if (PegaNumeroCasa(ref cadastroDoUsuario.NumeroDaCasa, "Digite o número da casa ou S para sair") == Resultado_e.Sair)
+                    return Resultado_e.Sair;
+                ListaUsuario_m.Add(cadastroDoUsuario);
+                return Resultado_e.Sucesso;
+            }
+            public static void GravaDados(string caminho, List<DadosDeUsuario_e> ListaUsuarios)
+            {
+                try
+                {
+                    string exportaCadastro = "";
+                    foreach (DadosDeUsuario_e dadosCadastro in ListaUsuarios)
+                    {
+                        exportaCadastro += deLimitdorInicio + "\r\n";
+                        exportaCadastro += tgNome + dadosCadastro.Nome + "\r\n";
+                        exportaCadastro += tgDtNascimento + dadosCadastro.DataDeNascimento + "\r\n";
+                        exportaCadastro += tgNomeDaRua + dadosCadastro.NomeDaRua + "\r\n";
+                        exportaCadastro += tgNumeroCasa + dadosCadastro.NumeroDaCasa + "\r\n";
+                        exportaCadastro += deLimitdorFim + "\r\n";
+
+                    }
+                    File.ReadAllLines(caminho);
+                    if (deLimitdorFim.Count() > 0)
+                        deLimitdorFim.Replace(deLimitdorFim, "\r\n");
+                    File.WriteAllText(caminho, exportaCadastro);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EXCEÇÃO: " + ex.Message);
+                }
+
+            }
+            public static void CarregaDados(string caminho, ref List<DadosDeUsuario_e> ListaUsuarios)
+            {
+                try
+                {
+                    if (File.Exists(caminho))
+                    {
+                        string[] conteudoRead = File.ReadAllLines(caminho);
+                        DadosDeUsuario_e dadosDoUsuario;
+                        dadosDoUsuario.Nome = "";
+                        dadosDoUsuario.DataDeNascimento = new DateTime();
+                        dadosDoUsuario.NomeDaRua = "";
+                        dadosDoUsuario.NumeroDaCasa = 0;
+                        foreach (string lines in conteudoRead)
+                        {
+                            if (lines.Contains(deLimitdorInicio))
+                                continue;
+                            if (lines.Contains(deLimitdorFim))
+                                ListaUsuarios.Add(dadosDoUsuario);
+                            if (lines.Contains(tgNome))
+                                dadosDoUsuario.Nome = lines.Replace(tgNome, "");
+                            if (lines.Contains(tgDtNascimento))
+                                dadosDoUsuario.DataDeNascimento = Convert.ToDateTime(lines.Replace(tgDtNascimento, ""));
+                            if (lines.Contains(tgNomeDaRua))
+                                dadosDoUsuario.NomeDaRua = lines.Replace(tgNomeDaRua, "");
+                            if (lines.Contains(tgNumeroCasa))
+                                dadosDoUsuario.NumeroDaCasa = Convert.ToUInt32(lines.Replace(tgNumeroCasa, ""));
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EXCEÇÃO: " + ex.Message);
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                List<DadosDeUsuario_e> ListaUsuarios = new List<DadosDeUsuario_e>();
+                string opcaoEscolhida = "";
+                deLimitdorInicio = "          ##### INICIO #####";
+                deLimitdorFim = "          ##### FIM #####";
+                tgNome = "NOME: ";
+                tgDtNascimento = "DATA_DE_NASCIMENTO: ";
+                tgNomeDaRua = "NOME_DA_RUA: ";
+                tgNumeroCasa = "NÚMERO_DA_CASA: ";
+                string caminhoDados = @"data_source.txt";
+                CarregaDados(caminhoDados, ref ListaUsuarios);
+
+                do
+                {
+                    ImprimeMensagens("Digite S para sair ou C para cadastrar um usuário");
+                    opcaoEscolhida = Console.ReadKey(true).KeyChar.ToString().ToLower();
+
+                    Console.Clear();
+                    if (opcaoEscolhida == "s")
+                    {
+                        ImprimeMensagens("Fim do programa");
+                        Console.ReadKey();
+                    }
+                    else if (opcaoEscolhida == "c")
+                    {
+                        //cadastra usuario
+                        if (CadastraUsuario(ref ListaUsuarios) == Resultado_e.Sucesso)
+                            GravaDados(caminhoDados, ListaUsuarios);
+                    }
+                    else
+                    {
+                        ImprimeMensagens("Opção Invalida!");
+                        Console.Clear();
+                    }
+
+                } while (opcaoEscolhida != "s");
+
+            }
+        }
+    }
+
+}
